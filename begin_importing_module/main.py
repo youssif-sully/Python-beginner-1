6ada78cdd6e392fa53b8965e9efcde9f37005664
youssif-sully <ymsyahia@hotmail.com>
2020-03-19 00:48:55 +0100

    update

---
+++ begin_importing_module/main.py
@@ -0,0 +1,11 @@
+# importing the color_module and naming it as col (short naming)
+import color_module as col
+
+
+print(col.var)
+
+
+message = "((RED-BR))Hello ((GREEN-BR))World((RESET))"
+print(col.colorText(message))
+
+col.meh()
---
+++ begin_importing_module/module_1.py
@@ -0,0 +1,10 @@
+print("Hello from module_1")
+print(
+"""
+The module contains:
+1. variable called var
+2. dictionary called colors
+3. cool function called colorText
+4. lame function called meh
+"""
+)
